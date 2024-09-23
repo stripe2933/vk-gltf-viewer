a62cdc7c569b409250d1a4308b6164594fcd0616
gomkyung2 <stripe2933@gmail.com>
2024-09-23 21:15:58 +0900

    WIP: workflow fix.

--- .github/workflows/clang-toolchain.cmake
+++ .github/workflows/clang-toolchain.cmake
@@ -2,3 +2,4 @@ set(CMAKE_C_COMPILER /usr/bin/clang-18)
 set(CMAKE_CXX_COMPILER /usr/bin/clang++-18)
 set(CMAKE_CXX_FLAGS "-stdlib=libc++")
 set(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++ -lc++abi")
+set(DBUS_SESSION_SOCKET_DIR /tmp) # https://github.com/microsoft/vcpkg/issues/40031
