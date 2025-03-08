e57b8ef58206bc971d0047bd54694f1f17070743
gomkyung2 <stripe2933@gmail.com>
2025-03-09 04:46:41 +0900

    Add macos-latest runner

---
+++ .github/workflows/scripts/macos-homebrew-clang-18-toolchain.cmake
@@ -0,0 +1,2 @@
+set(CMAKE_C_COMPILER /opt/homebrew/opt/llvm@18/bin/clang)
+set(CMAKE_CXX_COMPILER /opt/homebrew/opt/llvm@18/bin/clang++)
--- .github/workflows/scripts/macos-homebrew-clang-toolchain.cmake
+++ .github/workflows/scripts/macos-homebrew-clang-toolchain.cmake
@@ -1,2 +1,2 @@
-set(CMAKE_C_COMPILER /opt/homebrew/opt/llvm@18/bin/clang)
-set(CMAKE_CXX_COMPILER /opt/homebrew/opt/llvm@18/bin/clang++)
+set(CMAKE_C_COMPILER /opt/homebrew/opt/llvm/bin/clang)
+set(CMAKE_CXX_COMPILER /opt/homebrew/opt/llvm/bin/clang++)
